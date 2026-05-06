0cd3498ead7e018b1b6aa26b8f9dd106240589b5
sneedcat <83077245+sneedcat@users.noreply.github.com>
2026-05-06 08:38:07 +0000

    ci: update opencode, helium, and flake lock

--- pkgs/helium.nix
+++ pkgs/helium.nix
@@ -2,11 +2,11 @@
 
 let
   pname = "helium";
-  version = "0.12.0.2";
+  version = "0.12.1.1";
 
   src = pkgs.fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    sha256 = "sha256-1iU+hhisAPF2hAgfFob6Oe54JXjwb6uftiERa4t+OoE=";
+    sha256 = "sha256-+UE+JqQtxbA5szPvAohapXlES21VBOdNsV6Ej1dRRfs=";
   };
 
   appImageContents = pkgs.appimageTools.extract {
--- pkgs/opencode.nix
+++ pkgs/opencode.nix
@@ -15,7 +15,7 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "1.14.37";
+  version = "1.14.39";
   src = fetchFromGitHub {
     owner = "anomalyco";
     repo = "opencode";
